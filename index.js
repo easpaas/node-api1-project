e7e15c8aea8a492624d758a19e6a8db3789cece1
evan <aspaasevan@gmail.com>
2020-05-04 15:23:39 -0700

    post adds user when given correct req.body

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const server = express();
 const port = 8080;
 
+// required to read json from the body of a HTTP request
+server.use(express.json());
+
 // From shortid npm package 
 var shortid = require('shortid');
 
@@ -16,8 +19,24 @@ let users = [
 
 server.get('/', (req, res) => res.send('Hello World!'));
 
+// Returns all users 
 server.get('/api/users/', (req, res) => {
   res.status(200).json(users);
 })
+
+// Add a new user
+server.post('/api/users/', (req, res) => {
+  let newUser = req.body;
+
+  // Assign newUser an id
+  newUser = Object.assign({id: shortid.generate()}, newUser);
+
+  // Check if status code is correct
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+})
+
+
 
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
