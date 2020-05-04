d8312dc8db49d82938bfe7c92a02014617c862f4
evan <aspaasevan@gmail.com>
2020-05-04 14:54:48 -0700

    getAllUsers endpoint returns all users

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const server = express();
 const port = 8080;
 
+// From shortid npm package 
+var shortid = require('shortid');
+
+// Global initalization of Users
+let users = [
+  {
+    id: shortid.generate(), 
+    name: "Jane Doe", 
+    bio: "Not Tarzan's Wife, another Jane",  
+  }
+]
+
 server.get('/', (req, res) => res.send('Hello World!'));
 
-
+server.get('/api/users/', (req, res) => {
+  res.status(200).json(users);
+})
 
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
