ba3f62af905a0d92b7613655f228790040c3d769
evan <aspaasevan@gmail.com>
2020-05-05 09:53:26 -0700

    data renders to screen. styles added

--- node-api1-project/src/App.js
+++ node-api1-project/src/App.js
@@ -4,23 +4,29 @@ import './App.css';
 
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/users')
       .then(res => {
         console.log(res.data);
-        // setData(res.data);
+        setData(res.data);
       })
       .catch(err => console.log(err));
-  })
+  }, [])
   return (
     <div className="App">
       <h1>User Data</h1>
-      <div className="user">
-        <h3>{data.name}</h3>
-        <p>{data.bio}</p>
-      </div>
+      {
+        data.map(user => {
+          return (
+            <div key={user.id} className="user">
+              <h3>{user.name}</h3>
+              <p>{user.bio}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
