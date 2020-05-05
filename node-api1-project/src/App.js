0370d061d1d41b518b5da9ddae2502975ff710c8
evan <aspaasevan@gmail.com>
2020-05-05 09:44:55 -0700

    stretch cors added and data brought into react app

--- node-api1-project/src/App.js
+++ node-api1-project/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/users')
+      .then(res => {
+        console.log(res.data);
+        // setData(res.data);
+      })
+      .catch(err => console.log(err));
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Data</h1>
+      <div className="user">
+        <h3>{data.name}</h3>
+        <p>{data.bio}</p>
+      </div>
     </div>
   );
 }
